4079f06b576792fc0531b242cf70bb1422d1aaf4
alvinle29 <bach938@gmail.com>
2022-07-16 01:59:47 +0300

    added Navbar

---
+++ src/components/Layout/Pagination.js
@@ -0,0 +1,23 @@
+const Pagination = ({ currentPage, maxPage, navigatePageHandler }) => {
+    return (
+        <footer className={"footer"}>
+            <button
+                onClick={() => navigatePageHandler(false)}
+                disabled={currentPage === 1}
+            >
+                -
+            </button>
+            <span className={"current-page"}>
+                {currentPage} / {maxPage}
+            </span>
+            <button
+                onClick={() => navigatePageHandler(true)}
+                disabled={currentPage === maxPage}
+            >
+                +
+            </button>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ src/components/Layout/navBar.js
@@ -0,0 +1,24 @@
+import { useRef } from "react"
+
+const NavBar = ({ search, reload }) => {
+	const searchRef = useRef('');
+
+	const submitSearchHandler = (e) => {
+		e.preventDefault()
+		submitSearch(searchRef.current.value)
+		searchRef.current.value = ''
+	}
+
+	return (
+		<header className="navBar">
+			<h2 className="title" onClick={reload}>
+				Movie Services
+			</h2>
+			<form onSubmit={submitSearchHandler}>
+				<input placeholder="Enter movie name" ref={searchRef}/>
+			</form>
+		</header>
+	)
+}
+
+export default NavBar
