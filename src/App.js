71ce7ffdde716378babbeeb47a0066b15c1c2166
alvinle29 <bach938@gmail.com>
2022-07-16 12:23:31 +0300

    added hooks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import { useState } from "react";
 import NavBar from "./components/Layout/NavBar";
 import Pagination from "./components/Layout/Pagination";
 
 function App() {
 
+  const [movies, setMovies] = useState([])
+  const [currentPage, setCurrentPage] = useState(1)
+  const [maxPage, setMaxPage] = useState(1)
+  const [isSearching, setIsSearching] = useState(false)
+  const [searchText, setSearchText] = useState('')
 
+  useEffect(() => {
+    let url;
+    let queryPage = '&page=' + currentPage.toString();
+
+    const sendRequestHandler = async (url) => {
+      let data = await sendRequest(url);
+      setMovies(data.results);
+      setMaxPage(data.total_pages);
+    };
+
+    if (isSearching) {
+      let queryText = '&query=' + searchText;
+      url = process.env.REACT_APP_SEARCH_API + queryText + queryPage;
+    } else {
+      url = process.env.REACT_APP_DISCOVER_API + queryPage;
+    }
+
+    sendRequestHandler(url);
+  }, [currentPage, isSearching, searchText, sendRequest]);
 
   return (
     <>
