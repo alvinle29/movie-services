abffc0e42de1cfb7fd9461be7e911aeeeaa22a0c
alvinle29 <bach938@gmail.com>
2022-08-29 16:00:58 +0300

    fix picture bugs

--- src/components/Layout/navBar.js
+++ src/components/Layout/navBar.js
@@ -1,18 +1,18 @@
 import { useRef } from 'react'
 import styles from './NavBar.module.css'
 
-const NavBar = ({ search, reload }) => {
+const NavBar = ({ submitSearch, reloadHandler }) => {
 	const searchRef = useRef('');
 
 	const submitSearchHandler = (e) => {
 		e.preventDefault()
-		search(searchRef.current.value)
+		submitSearch(searchRef.current.value)
 		searchRef.current.value = ''
 	}
 
 	return (
 		<header className={styles.navBar}>
-			<h2 className={styles.title} onClick={reload}>
+			<h2 className={styles.title} onClick={reloadHandler}>
 				Movie Services
 			</h2>
 			<form onSubmit={submitSearchHandler}>
